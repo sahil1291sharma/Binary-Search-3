613535c46330df492ee8a07e420c8b87e0a80eee
sahil1291sharma <sahil1291sharma@users.noreply.github.com>
2021-07-11 05:00:53 -0400

    k closest two pointer

---
+++ problem3-two-pointer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        start = 0
+        end = len(arr)-1
+        
+        while (end - start) > k:
+            distLow = abs(x-arr[start])
+            distHigh = abs(x-arr[end])
+            if distLow >= distHigh:
+                start +=1
+            else:
+                end -=1
+        result =[]        
+        for i in range(start, end):
+            result.append(arr[i])
+        return result
